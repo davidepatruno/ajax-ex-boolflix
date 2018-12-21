91d9783c4260ff87dd799dd2959d8359796ecf6e
davidepatruno <patrunodavid@gmail.com>
2018-12-21 10:02:53 +0100

    rating stelle

---
+++ main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+  $('#mybutton').click(function(){
+    var searchValue = $('#search').val();
+    $('.wrapper_schede').html('');
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/movie',
+      method: 'GET',
+      data: {
+        api_key: '77e9ef84dffb1c10523baea7f48cdf44',
+        language: 'it',
+        query: searchValue,
+      },
+      success: function(data){
+        var movies = data.results;
+        for(var i = 0; i < movies.length; i++){
+          var movie = movies[i];
+          console.log(movies[i]);
+          var voto = Math.ceil((movie.vote_average) /2);
+          var lingua = movie.original_language;
+          var source = $('#movie-template').html();
+          var template = Handlebars.compile(source);
+          var context =
+          {
+            title: movie.title,
+            original_title: movie.original_title,
+            language: lingua,
+            // language: bandieraPerLingua(language),
+            rating: ratingInStelle(voto),
+          };
+          var html    = template(context);
+          $('.wrapper_schede').append(html);
+        }
+
+      },
+      error: function(){
+        alert("qualcosa non funziona");
+      },
+    });
+  });
+});
+
+// function bandieraPerLingua(lingua) {
+//   var htmlOutput = '';
+//   if (lingua == "en") {
+//     alert("inglese");
+//   }
+//
+//   return htmlOutput;
+// }
+
+function ratingInStelle(voto) {
+  var htmlOutput = '';
+  for (var i = 0; i < 5; i++){
+    if (i < voto){
+      htmlOutput += "<i class='fas fa-star'></i>"
+    }
+    else {
+      htmlOutput += "<i class='far fa-star'></i>"
+    }
+  }
+  return htmlOutput;
+}
